45743d4219ba8c75a337b0d675ce38b8c028a6fd
Gabriel Fornaeus <gf@hax0r.se>
2014-11-23 09:39:37 +0100

    Initial commit

---
+++ sakletaren.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python2
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2014 Gabriel Fornaeus <gf@hax0r.se>
+#
+# Distributed under terms of the GPLv3 license.
+
+"""
+A silly app for finding stuff
+"""
+
+# {{{ Libraries and global settings
+
+from flask import Flask
+
+app = Flask(__name__)
+# }}}
+
+@app.route('/')
+def index():
+    return "Sakletaren"
+
+if __name == '__main__':
+    app.run()
