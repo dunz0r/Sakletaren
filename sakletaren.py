23825ffa31269eda19b2adacaf3bea832e558af4
Gabriel Fornaeus <gf@hax0r.se>
2014-11-23 15:10:59 +0100

    Shiiiit

--- sakletaren.py
+++ sakletaren.py
@@ -12,7 +12,7 @@ A silly app for finding stuff
 # {{{ Libraries and global settings
 
 from flask import Flask, render_template, request, redirect
-from wtforms import Form, RadioField, TextField, TextAreaField, SelectField
+from wtforms import Form, SelectField
 import logging
 from logging.handlers import RotatingFileHandler
 
@@ -26,80 +26,87 @@ class pages():
     def Size():
         global sak
         redir = '/colour'
+        route = '/'
         form = sizeForm(request.form)
         if request.method == 'POST':
-            sak = sak + form.size
+            sak = sak + form.size.data
             return redirect(redir)
-        return render_template('template.html', form=form, redir=redir, field=form.size, label=form.size.label)
+        return render_template('template.html', form=form, redir=route, field=form.size, label=form.size.label)
 
     @app.route('/colour', methods = ['GET', 'POST'])
     def Colour():
         global sak
+        route = '/colour'
         redir = '/importance'
         form = colourForm(request.form)
         if request.method == 'POST':
-            sak = sak + form.colour
+            sak = sak + form.colour.data
             return redirect(redir)
-        return render_template('template.html', form=form, redir=redir, field=form.colour, label=form.colour.label)
+        return render_template('template.html', form=form, redir=route, field=form.colour, label=form.colour.label)
 
     @app.route('/importance', methods = ['GET', 'POST'])
     def Importance():
         global sak
+        route = 'importance'
         redir = '/electronic'
         form = importanceForm(request.form)
         if request.method == 'POST':
-            sak = sak + form.importance
+            sak = sak + form.importance.data
             return redirect(redir)
-        return render_template('template.html', form=form, redir=redir, field=form.importance, label=form.importance.label)
+        return render_template('template.html', form=form, redir=route, field=form.importance, label=form.importance.label)
 
     @app.route('/electronic', methods = ['GET', 'POST'])
     def Electronic():
         global sak
+        route = '/electronic'
         redir = '/lastseen'
         form = electronicForm(request.form)
         if request.method == 'POST':
-            sak = sak + form.electronic
+            sak = sak + form.electronic.data
             return redirect(redir)
-        return render_template('template.html', form=form, redir=redir, field=form.electronic, label=form.electronic.label)
+        return render_template('template.html', form=form, redir=route, field=form.electronic, label=form.electronic.label)
 
 
     @app.route('/lastseen', methods = ['GET', 'POST'])
     def LastSeen():
         global sak
+        route = '/lastseen'
         redir = '/outin'
         form = lastSeenForm(request.form)
         if request.method == 'POST':
-            sak = sak + form.lastSeen
+            sak = sak + form.lastSeen.data
             return redirect(redir)
-        return render_template('template.html', form=form, redir=redir, field=form.lastSeen, label=form.lastSeen.label)
+        return render_template('template.html', form=form, redir=route, field=form.lastSeen, label=form.lastSeen.label)
 
     @app.route('/outin', methods = ['GET', 'POST'])
     def OutIn():
         global sak
+        route = '/outin'
         redir = '/material'
         form = outInForm(request.form)
         if request.method == 'POST':
-            sak = sak + form.outIn
+            sak = sak + form.outIn.data
             return redirect(redir)
-        return render_template('template.html', form=form, redir=redir, field=form.outIn, label=form.outIn.label)
+        return render_template('template.html', form=form, redir=route, field=form.outIn, label=form.outIn.label)
 
     @app.route('/material', methods = ['GET', 'POST'])
     def Material():
         global sak
+        route = '/material'
         redir = '/found'
         form = materialForm(request.form)
         if request.method == 'POST':
             app.logger.info(form.material)
-            sak = sak + form.material
+            sak = sak + form.material.data
             app.logger.info(sak['material'])
             return redirect(redir)
-        return render_template('template.html', form=form, redir=redir, field=form.material, label=form.material.label)
+        return render_template('template.html', form=form, redir=route, field=form.material, label=form.material.label)
 
     @app.route('/found', methods = ['GET', 'POST'])
     def Found():
         global sak
         app.logger.info(unicode(sak))
-        return render_template('found.html', sak=sak, label=u'Jag vet vart den är, den är;')
+        return render_template('found.html', thing=sak, label=u'Jag vet vart den är, den är;')
         # TODO return funny result
 
 # }}}
@@ -119,7 +126,7 @@ class importanceForm(Form):
 
 class electronicForm(Form):
     electronicChoices = [('1', u'Nej'), ('2', u'Kanske')]
-    electronic = RadioField(u'Är den elektronisk?', choices=electronicChoices)
+    electronic = SelectField(u'Är den elektronisk?', choices=electronicChoices)
 
 class lastSeenForm(Form):
     lastSeenChoices = [('1', u'Igår'), ('2', u'Förra måndagen'), ('3', u'Förut')]
@@ -127,7 +134,7 @@ class lastSeenForm(Form):
 
 class outInForm(Form):
     outInChoices = [('1', u'Ute'), ('2', u'Inne')]
-    outIn = RadioField(u'Hade du den ute eller inne?', choices=outInChoices)
+    outIn = SelectField(u'Hade du den ute eller inne?', choices=outInChoices)
 
 class materialForm(Form):
     materialChoices = [('1', u'Cederträ'), ('2', u'Betong'), ('3', u'Plast'), ('4', u'Fryst kaffe'), ('5', u'Kolfiber'), ('6', u'Titan'), ('7', 'Ull')]
